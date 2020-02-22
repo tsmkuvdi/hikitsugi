85ae3b75e5c115c53d708e48fc05be27a211d9be
pvsknqyx <ntshp888pachimobile@gmail.com>
2020-02-22 20:17:18 +0900

    データベース作成時の注意記載

--- sourcecode/config/db_config.php
+++ sourcecode/config/db_config.php
@@ -1,5 +1,5 @@
-<?php
-$user = "root";
+﻿<?php
+$user = "root";  //mariadb作成時のuser,pass
 $pass = "toor";
-$databasename = "hikitsugi_db";
+$databasename = "hikitsugi_db";   //utf8_general_ci
 $dbtablename = "table_hikitsugi";
